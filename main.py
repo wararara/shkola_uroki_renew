886b707c0504e7526474609c7d1580c256731484
wararara <bgl.denis@gmail.com>
2024-03-04 23:39:19 +0300

    NUMERO DOS

--- main.py
+++ main.py
@@ -1,23 +1,9 @@
-
-
 class Student:
-
     aidi = 0
     name = ''
     title = ''
     klass = ''
     score = ''
-
-def vstavit(STUD):
-    for i in range (len(STUD)):
-        j = i -1
-        f = STUD[i]
-        while STUD[j].score > f.score and j >= 0:
-            STUD[j + 1] = STUD[j]
-            j -= 1
-        STUD[j + 1] = f
-
-    return STUD
 
 f = open('students.csv', 'r', encoding = 'utf8')
 f.readline()
@@ -32,19 +18,20 @@ for i in f:
     st.score = i.split(',')[4]
     STUD.append(st)
 
-vstavit(STUD)
+def find(STUD, key):
+    for i in range (len(STUD)):
+        if STUD[i].aidi == key:
+            ifamilia = str((STUD[i].name[1])[0] + '. ' + STUD[i].name[0])
+            return [STUD[i].title, ifamilia, STUD[i].score]
+    return -1
 
-print("10 класс:")
+inpat = input()
 
-z = 0
-for i in range (len(STUD)):
-    if '5' in STUD[i].score and '10' in STUD[i].klass:
-        if z == 0:
-            print('1 место: ', (STUD[i].name[1])[0], '. ', STUD[i].name[0])
-        if z == 1:
-            print('2 место: ', (STUD[i].name[1])[0], '. ', STUD[i].name[0])
-        if z == 2:
-            print('3 место: ', (STUD[i].name[1])[0], '. ', STUD[i].name[0])
-        if z == 3:
-            exit()
-        z += 1
+while inpat != 'СТОП':
+    r = find(STUD, inpat)
+    if r == -1:
+        print('Ничего не найдено')
+    else:
+        print('Проект № ', r[0], 'делал: ', r[1], 'он(а) получил(а) оценку - ', r[2])
+
+    inpat = input()
