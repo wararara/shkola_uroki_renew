2d01887b29cead976dbaa5ba2e0f366576d1d24f
belohovskiidv <bgl.denis@gmail.com>
2024-03-05 12:06:46 +0300

    N1 DONE

--- main.py
+++ main.py
@@ -1,55 +1,30 @@
-import secrets
+class Studentiki:
+    pass
 
-class Student:
-
-    aidi = 0
-    name = ''
-    title = ''
-    klass = ''
-    score = ''
-    login = ''
-    password = ''
-
-f = open('students.csv', 'r', encoding = 'utf8')
-holycow = f.readline().strip()
-STUD = []
-z = 0
+students = []
+f = open('students.csv')
+name = f.readline().strip()
+j = 0
 for i in f:
-    st = Student()
-    st.aidi = i.split(',')[0].strip()
-    st.name = (i.split(',')[1]).split()
-    st.title = i.split(',')[2]
-    st.klass = i.split(',')[3]
-    st.score = i.split(',')[4].strip()
-    STUD.append(st)
-
-bank = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
-def disaster(person):
-    t = ''
-    while (any(chr.isdigit() for chr in t) == False) and (any(chr.islower() for chr in t) == False) and (any(chr.isupper() for chr in t) == False):
-        t = ''
-        for i in range (8):
-            t += secrets.choice(bank)
-
-    return t.strip()
-
-def namemaking(person):
-    hi = person.name
-
-    login = str(hi[0]) + '_' + str(hi[1][0]) + str(hi[2][0])
-    return login.strip()
-
-for i in range (len(STUD)):
-    STUD[i].login = namemaking(STUD[i])
-    STUD[i].password = disaster(STUD[i])
-
-out = open('students_password.csv', 'w')
-print(holycow, file = out, end = '')
-whoa = ',login,password'
-print(whoa, file = out)
-
-for i in range (len(STUD)):
-    print(STUD[i].aidi,',',STUD[i].name[0] + ' ' + STUD[i].name[1] + ' ' + STUD[i].name[2],',',STUD[i].title,',',STUD[i].klass,',',STUD[i].score,',',STUD[i].login,',',STUD[i].password, file = out, end = '')
-    print(file = out)
-
-out.close()
+    s = i.strip().split(',')
+    students.append(Studentiki())
+    students[j].userid = s[0]
+    students[j].fio = s[1]
+    students[j].title = s[2]
+    students[j].clas = s[3]
+    students[j].score = s[4]
+    j += 1
+for i in students:
+    s = 0
+    k = 0
+    if i.score == 'None':
+        for j in students:
+            if j.clas == i.clas and j.score!='None':
+                k += 1
+                s += int(j.score)
+        i.score = f'{s/k:.3f}'
+f = open('students_new.csv','w')
+print(name, file=f)
+for i in students:
+    print(i.userid,',',i.fio,',',i.title,',',i.clas,',',i.score,file=f)
+f.close()
